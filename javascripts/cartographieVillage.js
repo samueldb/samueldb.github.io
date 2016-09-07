c2e0e8626bfd41d46cc19f8fd12e985c56b6e26c
= <=>
2016-09-07 16:55:25 +0200

    carte avec couche mapbox et layer switcher

--- javascripts/cartographieVillage.js
+++ javascripts/cartographieVillage.js
@@ -36,20 +36,65 @@ function init() {
           }
         }]
         };
+        
+        // Base Layers : 
+        var baseLayers = new ol.layer.Group(
+            {   title: 'fond de plan',
+            openInLayerSwitcher: true,
+            layers:
+                [// new ol.layer.Tile(
+                    // {	title: "Watercolor",
+                        // baseLayer: true,
+                        // source: new ol.source.Stamen({
+                        // layer: 'watercolor',
+                        // visible: false
+                      // })
+                    // }),
+                // new ol.layer.Tile(
+                    // {	title: "Toner",
+                        // baseLayer: true,
+                        // visible: false,
+                        // source: new ol.source.Stamen({
+                        // layer: 'toner'
+                      // })
+                    // }),
+                new ol.layer.Tile(
+                    {	title: "OSM",
+                        baseLayer: true,
+                        source: new ol.source.OSM(),
+                        visible: true
+                    })
+                ]
+        });
 
         var cartoDBSource = new ol.source.CartoDB({
         account: 'samueldeschampsberger',
         config: mapConfig
         });
+        
+        var mtft_1728Layer = new ol.layer.Tile({
+          source: new ol.source.XYZ({
+            url: 'https://api.mapbox.com/v4/samueldb.5ajstwl3/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2FtdWVsZGIiLCJhIjoiY2lzc2t4a3RnMDAwYTJ5bnplNjBiYXg4dyJ9.D9yc49jOivEKLDNmFaqIeg'
+          }),
+          title: "mappe Sarde de 1728"
+        })
+        var mtft_1888Layer = new ol.layer.Tile({
+          source: new ol.source.XYZ({
+            url: 'https://api.mapbox.com/v4/samueldb.5vy14jxi/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2FtdWVsZGIiLCJhIjoiY2lzc2t4a3RnMDAwYTJ5bnplNjBiYXg4dyJ9.D9yc49jOivEKLDNmFaqIeg'
+          }),
+          title: "cadastre de 1888"
+        })
+        
         var cartoDBLayer = new ol.layer.Tile({
-            source: cartoDBSource
+            source: cartoDBSource,
+            title: "batiments actuels"
           })
         var map = new ol.Map({
         layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          }),
-          cartoDBLayer
+          baseLayers,
+          mtft_1728Layer,
+          mtft_1888Layer,
+          cartoDBLayer,
         ],
         target: 'cartodb-map',
         view: new ol.View({
@@ -57,13 +102,16 @@ function init() {
           zoom: 16
         })
         });
-        
-            var extent = [977450.622600, 6492347.685738, 979924.444146, 6495803.182500];
-            var imgWidth = 9945;
-            var imgHeight = 6846;
-            var url = 'C:\\Users\\SDeschamps-berger\\Desktop\\perso\\projet carto\\genea\\git depot\\tiles\\mtft_1728\\mtft_1728_tiles\\_alllayers';
-            var crossOrigin = 'anonymous';
-            
+        // Add a layer switcher outside the map
+        var switcher = new ol.control.LayerSwitcher(
+            {	target:$(".layerSwitcher").get(0), 
+                show_progress:true,
+                extent: true,
+                trash: true,
+                oninfo: function (l) { alert(l.get("title")); }
+            });
+        map.addControl(switcher);
+
         
         //var extent = map.layers[1].getSource().getExtent();
         //map.getView().fit(extent, map.getSize());
