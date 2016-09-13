839481e0e0df9e063b04b67de3c8e607e6a0d8ac
= <=>
2016-09-13 16:46:36 +0200

    ajout des batiments 1888

--- javascripts/cartographieVillage.js
+++ javascripts/cartographieVillage.js
@@ -105,17 +105,22 @@ function init() {
           }),
           title: "cadastre de 1888"
         })
+        var mtft_ActuLayer = new ol.layer.Tile({
+          // extent: [-13884991, 2870341, -7455066, 6338219],
+          source: new ol.source.TileWMS({
+            url: 'http://inspire.cadastre.gouv.fr/scpc/73253.wms?service=WMS&request=GetMap',
+            params: {'LAYERS': 'LIEUDIT,CP.CadastralParcel,CLOTURE,DETAIL_TOPO,HYDRO,VOIE_COMMUNICATION,BU.Building,BORNE_REPERE',
+                     'VERSION':'1.3',
+                     'FORMAT':'image/png',
+                     'WIDTH':'256',
+                     'HEIGHT':'256',
+                     'CRS':'EPSG:3857',
+                     // 'BBOX':'-13884991, 2870341, -7455066, 6338219'
+                    },
+          }),
+          title: 'cadastre actuel'
+        })
         
-        // var cartoDBLayerActuel = new ol.layer.Tile({
-            // source: cartoDBSourceActuel,
-            // title: "batiments actuels"
-          // })
-        // var cartoDBLayer1728 = new ol.layer.Tile({
-            // source: cartoDBSource1728,
-            // title: "batiments 1728",
-            // crossOrigin: 'anonymous'
-          // })
-          
           var view = new ol.View({
           center: ol.proj.fromLonLat([6.5688, 45.48978]),
           zoom: 16
@@ -151,6 +156,35 @@ function init() {
             }
         });
         // Batiments 1888
+        var batiments1888 = new ol.layer.Vector({
+            source: new ol.source.Vector({
+              url: 'https://samueldeschampsberger.cartodb.com/api/v2/sql/?format=GeoJSON&q=SELECT%20*%20FROM%20maisons_1888&format_output=json',
+              format: new ol.format.GeoJSON(),
+              // type:"FeatureCollection",
+            }),
+            title: 'batiments 1888',
+            style: function(feature, resolution) {
+                var strokecolor = 'red';
+                if(feature.get('utilisation') == 'grange') {
+                    strokecolor = '#1F78B4';
+                  } else if(feature.get('utilisation') == 'habitation') {
+                    strokecolor = '#B2DF8A';
+                  } else if(feature.get('utilisation') == 'masure') {
+                    strokecolor = '#33A02C';
+                  } else {
+                    strokecolor = 'orange';
+                  }
+                  return new ol.style.Style({
+                    fill: new ol.style.Fill({
+                      color: strokecolor
+                    }),
+                    stroke: new ol.style.Stroke({
+                      color: [220,220,220,1],
+                      width: 1
+                    })})
+            }
+        });
+        // Batiments Actuels
         var batimentsActuels = new ol.layer.Vector({
             source: new ol.source.Vector({
               url: 'https://samueldeschampsberger.cartodb.com/api/v2/sql/?format=GeoJSON&q=SELECT%20*%20FROM%20maisons&format_output=json',
@@ -180,13 +214,40 @@ function init() {
             }
         });
         
+        // Layers 1728 : 
+        var layers1728 = new ol.layer.Group(
+            {   title: '1728',
+            openInLayerSwitcher: true,
+            layers:
+                [mtft_1728Layer,
+                batiments1728
+                ]
+        });
+        // Layers 1888 : 
+        var layers1888 = new ol.layer.Group(
+            {   title: '1888',
+            openInLayerSwitcher: true,
+            layers:
+                [mtft_1888Layer,
+                batiments1888
+                ]
+        });
+        // Layers Actuels : 
+        var layersActu = new ol.layer.Group(
+            {   title: 'Actuels',
+            openInLayerSwitcher: true,
+            layers:
+                [mtft_ActuLayer,
+                batimentsActuels
+                ]
+        });
+        
         var map = new ol.Map({
         layers: [
           baseLayers,
-          mtft_1728Layer,
-          mtft_1888Layer,
-          batimentsActuels,
-          batiments1728
+          layers1728,
+          layers1888,
+          layersActu
         ],
         target: 'cartodb-map',
         view: view
